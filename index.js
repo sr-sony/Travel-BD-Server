6a202e6d862937267d9ec00b25ce941135d13352
sr-sony <sr.sony.05@gmail.com>
2022-11-09 20:17:56 +0600

    mongo

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const port = process.env.PORT || 5000;
+require('dotenv').config();
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.spo52sg.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+  try{
+    const serviceCollection = client.db('travelBd').collection('services');
+
+    app.get('/services', async(req,res)=>{
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services)
+    })
+  }
+  finally{
+
+  }
+
+}
+
+run().catch(error=>console.error(error));
+
+
+app.get("/", (req, res) => {
+  res.send("server running");
+});
+
+app.listen(port, () => {
+  console.log("server listening on port", port);
+});
