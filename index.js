76d08474e70a681bef12dd145a4aef802d21c501
sr-sony <sr.sony.05@gmail.com>
2022-11-10 20:45:59 +0600

    review api

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json());
 
+//mongodb uri
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.spo52sg.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
